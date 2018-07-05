3f6bbcfe6170f1388ec2d2a7fe2767fea657b794
Erin Woo <erinpwoo@yahoo.com>
2018-07-05 14:07:27 -0700

    Imported Leap Motion Interaction Engine assets

---
+++ Assets/Scripts/IndexCollisionScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndexCollisionScript : MonoBehaviour {
+
+    void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name + " has collided");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Button")
+        {
+            Debug.Log(collision.transform.name + " has collided");
+        }
+       
+    }
+
+}
