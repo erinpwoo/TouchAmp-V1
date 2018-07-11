35df45e2ccb12584e8d540f3c3c68ce339da8bf2
Erin Woo <erinpwoo@yahoo.com>
2018-07-11 11:46:21 -0700

    MaxMSP setup

---
+++ Assets/Scripts/OSCController.cs
@@ -0,0 +1,32 @@
+﻿
+
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityOSC;
+    
+    public class OSCController : MonoBehaviour
+    {
+        
+        public string serverId = "MaxMSP";
+        public string serverIp = "127.0.0.1";
+        public int serverPort = 7400;
+        // Use this for initialization
+        void Start()
+        {
+            OSCHandler.Instance.Init(this.serverId, this.serverIp, this.serverPort);
+        }
+
+        public KeyCode debugKey = KeyCode.S;
+        public string debugMessage = "/sample";
+   
+    // Update is called once per frame
+    void Update()
+        {
+            if (Input.GetKeyDown(this.debugKey))
+            {
+                OSCHandler.Instance.SendMessageToClient(this.serverId, this.debugMessage,34); //to change preset number, timeSinceLevelLoad
+            }
+        }
+    }
+
