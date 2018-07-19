df1f1fcbb49c4c8737e84f89b8a4a9e9141e220c
Erin Woo <erinpwoo@yahoo.com>
2018-07-18 17:48:59 -0700

    Writing gameplay

--- Assets/LeapMotion/Modules/InteractionEngine/Scripts/UI/InteractionButton.cs
+++ Assets/LeapMotion/Modules/InteractionEngine/Scripts/UI/InteractionButton.cs
@@ -21,6 +21,7 @@ namespace Leap.Unity.Interaction {
   ///</summary>
   public class InteractionButton : InteractionBehaviour {
 
+    public int buttonNum;
     #region Inspector
 
     [Header("UI Control")]
@@ -550,11 +551,47 @@ namespace Leap.Unity.Interaction {
             }
     }
 
-    public void ButtonReact(float velocity) //writing function to represent button shake
+        void lightUp() //lights up individual button when called upon
         {
-
+            string buttonTag = this.name;
+            Color color;
+            switch (buttonTag)
+            {
+                case "Orange Button":
+                    color = new Color(.2F, .3F, .4F);
+                    break;
+                case "Green Button":
+                    color = Color.green;
+                    break;
+                case "Red Button":
+                    color = Color.red;
+                    break;
+                case "Yellow Button":
+                    color = Color.yellow;
+                    break;
+                case "Light Blue Button":
+                    color = Color.cyan;
+                    break;
+                case "Grey Button":
+                    color = Color.grey;
+                    break;
+                case "Pink Button":
+                    color = new Color(1F, .71F, .76F);
+                    break;
+                case "Blue Button":
+                    color = Color.blue;
+                    break;
+                case "Purple Button":
+                    color = new Color(.58F, 0, .83F);
+                    break;
+                default: //default preset: 36
+                    color = Color.clear;
+                    break;
+            }
+            Material material = GetComponent<Renderer>().material;
+            material.SetColor("_EmissionColor", color);
         }
-    #endregion
+        #endregion
 
-  }
+    }
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,15 +4,63 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
-    private static GameController instance = null;
+    int roundNum;
+    bool isPlaying;
 
     // Use this for initialization
     void InitGame () {
-		
+        roundNum = 1;
+        isPlaying = true;
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    void playRound(int roundNum) //randomly generates and executes pattern 
+    {
+
+    }
+
+    void lightUp() //lights up individual button when called upon -- NEEDS FIXING
+    {
+            string buttonTag = this.name;
+            Color color;
+            switch (buttonTag)
+            {
+                case "Orange Button":
+                    color = new Color(.2F, .3F, .4F);
+                    break;
+                case "Green Button":
+                    color = Color.green;
+                    break;
+                case "Red Button":
+                    color = Color.red;  
+                    break;
+                case "Yellow Button":
+                    color = Color.yellow;
+                    break;
+                case "Light Blue Button":
+                    color = Color.cyan;
+                    break;
+                case "Grey Button":
+                    color = Color.grey;
+                    break;
+                case "Pink Button":
+                    color = new Color(1F, .71F, .76F);
+                    break;
+                case "Blue Button":
+                    color = Color.blue;
+                    break;
+                case "Purple Button":
+                    color = new Color(.58F,0, .83F);
+                    break;
+                default: //default preset: 36
+                    color = Color.clear;
+                    break;
+            }
+            Material material = GetComponent<Renderer>().material;
+            material.SetColor("_EmissionColor", color);
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 
