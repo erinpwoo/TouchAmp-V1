0dd31133ae069a655cea8d7437a8cb06ab643412
Erin Woo <erinpwoo@yahoo.com>
2018-07-19 16:29:02 -0700

    playRound() + LightUp() implementation together

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,15 +7,19 @@ namespace Leap.Unity.Interaction
     public class GameController : MonoBehaviour
     {
 
+        //BUTTON INDICES:
+        //Green = 0; Blue = 1; Yellow = 2; Light blue = 3; Pink = 4; Purple = 5; Red = 6; Grey = 7; Orange = 8
+
         int roundNum;
         bool isPlaying;
-        public GameObject[] buttons;
+        public GameObject[] buttons; //fixed array of button indecies
+        List<int> pattern = new List<int>(); //pattern 
         
 
         // Use this for initialization
         void Start()
         {
-            roundNum = 1;
+            roundNum = 0;
             isPlaying = true;
             buttons = GameObject.FindGameObjectsWithTag("Button");
             for (int i = 0; i < buttons.Length; i ++)
@@ -24,9 +28,13 @@ namespace Leap.Unity.Interaction
             }
         }
 
-        void playRound(int roundNum) //randomly generates and executes pattern 
+        void playRound() //randomly generates and executes pattern 
         {
-
+            pattern.Add(Random.Range(0, 7));
+            for (int i = 0; i < roundNum; i++)
+            {
+                buttons[pattern[i]].GetComponent<InteractionButton>().LightUp(); //selects Interaction button, applies LightUp()
+            }
         }
 
         // Update is called once per frame
