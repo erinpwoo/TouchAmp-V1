bec4d202c3ee7ea6381e9dda72aa1e6e6a76704f
Erin Woo <erinpwoo@yahoo.com>
2018-07-19 15:44:09 -0700

    Object class design

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -2,15 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ButtonController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+namespace Leap.Unity.Interaction
+{
+    public class ButtonController : MonoBehaviour
+    {
+        private InteractionButton button;
+        bool lightUp;
+
+        void Start()
+        {
+            button = GetComponent<InteractionButton>();
+            lightUp = false;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void LightUp() //lights up individual button when called upon -- NEEDS FIXING
+        {
+            string buttonTag = this.name;
+            Color color;
+            switch (buttonTag)
+            {
+                case "Orange Button":
+                    color = new Color(.2F, .3F, .4F);
+                    break;
+                case "Green Button":
+                    color = Color.green;
+                    break;
+                case "Red Button":
+                    color = Color.red;
+                    break;
+                case "Yellow Button":
+                    color = Color.yellow;
+                    break;
+                case "Light Blue Button":
+                    color = Color.cyan;
+                    break;
+                case "Grey Button":
+                    color = Color.grey;
+                    break;
+                case "Pink Button":
+                    color = new Color(1F, .71F, .76F);
+                    break;
+                case "Blue Button":
+                    color = Color.blue;
+                    break;
+                case "Purple Button":
+                    color = new Color(.58F, 0, .83F);
+                    break;
+                default: //default preset: 36
+                    color = Color.clear;
+                    break;
+            }
+            Material material = GetComponent<Renderer>().material;
+            material.SetColor("_EmissionColor", color);
+        }
+    }
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -18,47 +18,6 @@ public class GameController : MonoBehaviour {
 
     }
 
-    void lightUp() //lights up individual button when called upon -- NEEDS FIXING
-    {
-            string buttonTag = this.name;
-            Color color;
-            switch (buttonTag)
-            {
-                case "Orange Button":
-                    color = new Color(.2F, .3F, .4F);
-                    break;
-                case "Green Button":
-                    color = Color.green;
-                    break;
-                case "Red Button":
-                    color = Color.red;  
-                    break;
-                case "Yellow Button":
-                    color = Color.yellow;
-                    break;
-                case "Light Blue Button":
-                    color = Color.cyan;
-                    break;
-                case "Grey Button":
-                    color = Color.grey;
-                    break;
-                case "Pink Button":
-                    color = new Color(1F, .71F, .76F);
-                    break;
-                case "Blue Button":
-                    color = Color.blue;
-                    break;
-                case "Purple Button":
-                    color = new Color(.58F,0, .83F);
-                    break;
-                default: //default preset: 36
-                    color = Color.clear;
-                    break;
-            }
-            Material material = GetComponent<Renderer>().material;
-            material.SetColor("_EmissionColor", color);
-    }
-
     // Update is called once per frame
     void Update () {
 		
