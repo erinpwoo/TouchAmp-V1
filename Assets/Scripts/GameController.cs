25309012d0e67a4dd977356cbbfb6ec0164b9523
Erin Woo <erinpwoo@yahoo.com>
2018-07-19 15:55:08 -0700

    Reads object number

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,25 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour {
-
-    int roundNum;
-    bool isPlaying;
-
-    // Use this for initialization
-    void InitGame () {
-        roundNum = 1;
-        isPlaying = true;
-	}
-
-    void playRound(int roundNum) //randomly generates and executes pattern 
+namespace Leap.Unity.Interaction
+{
+    public class GameController : MonoBehaviour
     {
 
+        int roundNum;
+        bool isPlaying;
+        public GameObject[] buttons;
+        
+
+        // Use this for initialization
+        void Start()
+        {
+            roundNum = 1;
+            isPlaying = true;
+            buttons = GameObject.FindGameObjectsWithTag("Button");
+            for (int i = 0; i < buttons.Length; i ++)
+            {
+                Debug.Log(i + " " + buttons[i].name);
+            }
+        }
+
+        void playRound(int roundNum) //randomly generates and executes pattern 
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
     }
-
-    // Update is called once per frame
-    void Update () {
-		
-	}
-
 }
