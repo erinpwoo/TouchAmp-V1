c29a1f384d31d25b79821ce4f484a73953d5febd
Erin Woo <erinpwoo@yahoo.com>
2018-07-25 13:10:12 -0700

    Collision light up!!!!!!!!!

--- Assets/Scripts/MaterialChange.cs
+++ Assets/Scripts/MaterialChange.cs
@@ -22,8 +22,23 @@ using UnityEngine;
             Debug.Log("Child material: " + original);
         }
 
-        // Update is called once per frame
-        void Update()
+        private void OnCollisionEnter(Collision collision)
+        {
+            child.GetComponent<Renderer>().material = emissive;
+        }
+
+        private void OnCollisionExit(Collision collision)
+        {
+            child.GetComponent<Renderer>().material = original;        
+        }
+
+        private void OnCollisionStay(Collision collision)
+        {
+            child.GetComponent<Renderer>().material = emissive;
+        }
+
+    // Update is called once per frame
+    void Update()
         {
             
         }
