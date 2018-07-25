b8f1e1aa04b4d25476aff84514178e399fa3d40f
Erin Woo <erinpwoo@yahoo.com>
2018-07-25 12:33:11 -0700

    Reads child material

---
+++ Assets/Scripts/MaterialChange.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+    public class MaterialChange : MonoBehaviour
+    {
+        public Material emissive;
+        Transform child;
+        Material original;
+
+        void Awake()
+        {
+            string buttonTag = this.name;
+            child = this.gameObject.transform.Find("Cube");
+            original = child.GetComponent<Renderer>().material;
+            
+        }
+        void Start()
+        {
+            Debug.Log("Name: " + child.name);
+            Debug.Log("Child material: " + original);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+
+    }
+
